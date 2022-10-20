c832a0ea885df346c72c605de54d73ec66648b41
Adam D <drozdowskiadam@gmail.com>
2022-10-20 22:50:19 +0200

    add createTollsArea cz2

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const prepareDOMElements = () => {
 }
 
 const prepareDOMEvents = () => {
-    addBtn.addEventListener('click', addNewTodo)
+    addBtn.addEventListener('click', addNewTodo);
 }
 
 
@@ -23,18 +23,36 @@ const addNewTodo = () => {
     if(todoInput.value !==''){
         newTodo = document.createElement('li');
         newTodo.textContent = todoInput.value;
+        createToolsArea();
         ulList.append(newTodo);
+
+
         todoInput.value = '';
         errorInfo.textContent = '';
     }else {
-        errorInfo.textContent = 'Wpisz treść zadania!'
+        errorInfo.textContent = 'Wpisz treść zadania!';
     }
 }
 
 const createToolsArea = () => {
-     const toolsPanel = document.createElement('div')
-     toolsPanel.classList.add('tools')
+     const toolsPanel = document.createElement('div');
+     toolsPanel.classList.add('tools');
+     newTodo.append(toolsPanel)
+
+     const completeBtn = document.createElement('button');
+     completeBtn.classList.add('complete');
+     completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+
+     const editBtn = document.createElement('button');
+     editBtn.classList.add('edit');
+     editBtn.textContent = 'EDIT';    
+
+     const deleteBtn = document.createElement('button');
+     deleteBtn.classList.add('delete');
+     deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+     toolsPanel.append(completeBtn, editBtn, deleteBtn);
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener('DOMContentLoaded', main);
 

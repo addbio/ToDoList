85552572c6a6e668712d338d323b3428e3353ebb
Adam D <drozdowskiadamu@gmail.com>
2022-10-21 16:14:19 +0200

    poprawa czxytelnosci kodu

--- script.js
+++ script.js
@@ -1,58 +1,53 @@
 let todoInput, errorInfo, addBtn, ulList, newTodo;
 
 const main = () => {
-    prepareDOMElements();
-    prepareDOMEvents();
-}
+  prepareDOMElements();
+  prepareDOMEvents();
+};
 
 const prepareDOMElements = () => {
-    todoInput = document.querySelector('.todo-input');
-    errorInfo = document.querySelector('.error-info');
-    addBtn = document.querySelector('.btn-add');
-    ulList = document.querySelector('.todolist ul');
-
-}
+  todoInput = document.querySelector(".todo-input");
+  errorInfo = document.querySelector(".error-info");
+  addBtn = document.querySelector(".btn-add");
+  ulList = document.querySelector(".todolist ul");
+};
 
 const prepareDOMEvents = () => {
-    addBtn.addEventListener('click', addNewTodo);
-}
-
-
+  addBtn.addEventListener("click", addNewTodo);
+};
 
 const addNewTodo = () => {
-    if(todoInput.value !==''){
-        newTodo = document.createElement('li');
-        newTodo.textContent = todoInput.value;
-        createToolsArea();
-        ulList.append(newTodo);
+  if (todoInput.value !== "") {
+    newTodo = document.createElement("li");
+    newTodo.textContent = todoInput.value;
+    createToolsArea();
+    ulList.append(newTodo);
 
-
-        todoInput.value = '';
-        errorInfo.textContent = '';
-    }else {
-        errorInfo.textContent = 'Wpisz treść zadania!';
-    }
-}
+    todoInput.value = "";
+    errorInfo.textContent = "";
+  } else {
+    errorInfo.textContent = "Wpisz treść zadania!";
+  }
+};
 
 const createToolsArea = () => {
-     const toolsPanel = document.createElement('div');
-     toolsPanel.classList.add('tools');
-     newTodo.append(toolsPanel)
+  const toolsPanel = document.createElement("div");
+  toolsPanel.classList.add("tools");
+  newTodo.append(toolsPanel);
 
-     const completeBtn = document.createElement('button');
-     completeBtn.classList.add('complete');
-     completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+  const completeBtn = document.createElement("button");
+  completeBtn.classList.add("complete");
+  completeBtn.innerHTML = '<i class="fas fa-check"></i>';
 
-     const editBtn = document.createElement('button');
-     editBtn.classList.add('edit');
-     editBtn.textContent = 'EDIT';    
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit");
+  editBtn.textContent = "EDIT";
 
-     const deleteBtn = document.createElement('button');
-     deleteBtn.classList.add('delete');
-     deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete");
+  deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
 
-     toolsPanel.append(completeBtn, editBtn, deleteBtn);
-}
+  toolsPanel.append(completeBtn, editBtn, deleteBtn);
+};
 
-document.addEventListener('DOMContentLoaded', main);
-
+document.addEventListener("DOMContentLoaded", main);
